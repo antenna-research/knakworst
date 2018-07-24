ca81d6f11974f0b60272cc0e0444553f588a71eb
Jackson Moore <jacksonmoore@tuta.io>
2018-07-24 14:02:45 +0200

    base markup for user profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,54 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 export default function Profile(props) {
 
-  return (<div>
-      <h3>{ showGuess( props.word, props.letters ) }</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input type="text" name="letter" onChange={handleChange} />
-        </label>
-      </form>
-      <h4> Wrong guesses so far: { wrongGuessCount( props.word, props.letters ) } </h4>
-      <button onClick={props.restart} >Play Again!</button>
+  // remove when container is ready
+  props.user = {
+    firstName: 'Kinney',
+    lastName: 'Tate',
+    age: 20,
+    phone: '974-566-3044',
+    email: 'Kinney@gmail.com',
+    address: 'Amsterdam',
+
+    profile: {
+      genres: ['Rock', 'Jazz', 'Pop'],
+      instrument: ['Guitar'],
+      youtube: 'ltmYirj5IA0',
+    },
+  }
+
+  return (<div class="profile">
+    <div class="profile-video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/{ props.user.profile.youtube }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+    <ul class="profile-genres">
+      {
+        props.profile.genres.map( (genre, i) => 
+        <li key={i}>{ genre }</li>)
+      }
+    </ul>
+    <div class="profile-name-age-place">
+      <div class="profile-name">
+        {props.user.firstName} {props.user.lastName}
+      </div>
+      <div class="profile-age">
+        {props.user.age}
+      </div>
+      <div class="profile-city">
+        {props.user.age}
+      </div>
+    </div>
+    <ul class="profile-instruments">
+      {
+        props.profile.instruments.map( (instrument, i) => 
+        <li key={i}>{ instrument }</li>)
+      }
+    </ul>
   </div>)
 
 }
+/*
+
+*/
