b0c77af8b0ab2d7d1da6df9e72ff8748040052bf
Jackson Moore <jacksonmoore@tuta.io>
2018-07-24 13:23:54 +0200

    add bootstrap package

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,16 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
+export default function Profile(props) {
+
+  return (<div>
+      <h3>{ showGuess( props.word, props.letters ) }</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" name="letter" onChange={handleChange} />
+        </label>
+      </form>
+      <h4> Wrong guesses so far: { wrongGuessCount( props.word, props.letters ) } </h4>
+      <button onClick={props.restart} >Play Again!</button>
+  </div>)
+
+}
