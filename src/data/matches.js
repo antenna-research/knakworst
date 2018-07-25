73d8434d539b0221a2e13fa9b1a020c5c7b5b0ae
Jackson Moore <jacksonmoore@tuta.io>
2018-07-25 10:59:48 +0200

    add better dummy data

--- src/data/matches.js
+++ src/data/matches.js
@@ -1,3 +1,4 @@
+
 export default {
   1: {
     likes: [2, 3],
@@ -10,8 +11,28 @@ export default {
     matches: [1]
   },
   3: {
-    likes: [1],
-    dislikes: [5, 6, 7],
-    matches: [1]
+    likes: [],
+    dislikes: [],
+    matches: []
+  },
+  4: {
+    likes: [],
+    dislikes: [],
+    matches: []
+  },
+  5: {
+    likes: [],
+    dislikes: [],
+    matches: []
+  },
+  6: {
+    likes: [],
+    dislikes: [],
+    matches: []
+  },
+  7: {
+    likes: [],
+    dislikes: [],
+    matches: []
   }
 }
