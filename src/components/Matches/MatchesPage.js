1887cf6fbb5ffb1d24adf4aee1be7426bd2d23ec
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-07-25 16:30:57 +0200

    setup mixin / styling in progress...

--- src/components/Matches/MatchesPage.js
+++ src/components/Matches/MatchesPage.js
@@ -9,44 +9,36 @@ import './styles/MatchesPage.scss'
 
 import PropTypes from 'prop-types'
 
-const tempStyle = {
-  border: '2px solid gray',
-  borderRadius: '20px',
-  // margin: '10%',
-  textAlign: 'center'
-}
-
 const MatchesPage = props => (
-  <Grid>
-    <Row className="show-grid">
-      {props.matchedUsers.map(user => (
-        <Col sm={6} md={3} key={user.id} style={tempStyle} id="MachesPage">
-          <Link to={`/profile/${user.id}`}>
-            <Button bsStyle="primary">Profile</Button>
-          </Link>
-          <h1>{user.username}</h1>
-          <p>age: {user.age}</p>
-          <p>place: {user.address}</p>
-          <p>genre(s): {user.genres.map(genre => <span key={genre}>{genre} </span>)}</p>
+  <div id="MachesPage">
+    {props.matchedUsers.map(user => (
+      <Col xs={12} md={5} key={user.id} className="profile-wrapper">
+        <h1 className="name">{user.username}</h1>
+        <p>age: {user.age}</p>
+        <p>location: {user.address}</p>
+        <p>genre(s): {user.genres.map(genre => <span key={genre}>{genre} </span>)}</p>
 
-          <p>
-            instrument(s):{' '}
-            {user.instruments.map(instrument => <span key={instrument}>{instrument}</span>)}
-          </p>
+        <p>
+          instrument(s):{' '}
+          {user.instruments.map(instrument => <span key={instrument}>{instrument} </span>)}
+        </p>
 
-          <hr />
-          <a
-            href={`https://wa.me/${user.phone.replace(
-              /-/g,
-              ''
-            )}?text=I'm%20matched%20with%20you%20on%20Knakworst`}
+        <Link to={`/profile/${user.id}`}>
+          <Button bsStyle="primary" className="button">
+            Profile
+          </Button>
+        </Link>
+        {/* <a
+          href={`https://wa.me/${user.phone.replace(
+            /-/g,
+            ''
+          )}?text=I'm%20matched%20with%20you%20on%20Knakworst`}
           >
-            <FontAwesomeIcon icon={['fab', 'whatsapp']} size="3x" />
-          </a>
-        </Col>
-      ))}
-    </Row>
-  </Grid>
+          <FontAwesomeIcon icon={['fab', 'whatsapp']} size="3x" />
+        </a> */}
+      </Col>
+    ))}
+  </div>
 )
 
 MatchesPage.propTypes = {}
