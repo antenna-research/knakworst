de9a03673dec2a00eb07048158fce92a17e8baf7
isabellabrookes <hello@isabellabrookes.com>
2018-07-25 16:47:40 +0200

    removed header and added classNames to buttons

--- src/components/Swipe/Swipe.js
+++ src/components/Swipe/Swipe.js
@@ -9,12 +9,11 @@ export default function swipe(props) {
   return (
     <div id={'swipe'}>
       {!props.currentUserId && !props.currentCandidateId && 'Loading'}
-      <h1>Swipe page is rendered</h1>
       <ProfileCard profile={currentCandidate} />
-      <button onClick={() => props.setDislikeUser(props.currentUserId, props.currentCandidateId)}>
+      <button className={'swipe-button'} onClick={() => props.setDislikeUser(props.currentUserId, props.currentCandidateId)}>
         <FontAwesomeIcon icon={'times-circle'} size="2x" />
       </button>
-      <button onClick={() => props.setLikeUser(props.currentUserId, props.currentCandidateId)}>
+      <button className={'swipe-button'} onClick={() => props.setLikeUser(props.currentUserId, props.currentCandidateId)}>
         <FontAwesomeIcon icon={'heart'} size="2x" />
       </button>
     </div>
