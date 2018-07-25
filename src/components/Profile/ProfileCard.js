029c60cb66bb6a67844235928d3602dccd2c9f99
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-07-25 12:38:16 +0200

    refactor profile and swipe component

--- src/components/Profile/ProfileCard.js
+++ src/components/Profile/ProfileCard.js
@@ -1,44 +1,38 @@
 import React from 'react'
 import './styles/ProfileCard.css'
 
-export default function ProfileCard(props) {
-  const currentUser = props.profile[props.userId]
-  if (!currentUser) return null
-  const userId = props.userId
-  const currentCandidate = props.profile[userId]
+export default function ProfileCard({ profile }) {
+  // const currentUser = props.profile[props.userId]
+  // if (!currentUser) return null
+  // const userId = props.userId
+  // const currentCandidate = props.profile[userId]
   return (
     <div id={'Profile-component'}>
       <div id={'Profile-card'}>
-        {!props.profile && 'Loading'}
+        {!profile && 'Loading'}
         <div class="profile-video">
-          <iframe src="https://www.youtube.com/embed/{ props.user.profile.youtube }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe
+            src="https://www.youtube.com/embed/{ props.user.profile.youtube }"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+          />
         </div>
 
-        <h1>{currentCandidate.username}</h1>
+        <h1>{profile.username}</h1>
 
-      <ul class="profile-genres">
-        {currentCandidate.genres.map( (genre, i) =>
-          <li key={i}>{ genre }</li>)
-        }
-      </ul>
-      <div class="profile-name-age-place">
-        <div class="profile-name">
-          {currentCandidate.firstName} {props.profile.lastName}
+        <ul class="profile-genres">{profile.genres.map((genre, i) => <li key={i}>{genre}</li>)}</ul>
+        <div class="profile-name-age-place">
+          <div class="profile-name">
+            {profile.firstName} {profile.lastName}
+          </div>
+          <div class="profile-age">{profile.age}</div>
+          <div class="profile-city">{profile.city}</div>
         </div>
-        <div class="profile-age">
-          {currentCandidate.age}
-        </div>
-        <div class="profile-city">
-          {currentCandidate.city}
-        </div>
+        <ul class="profile-instruments">
+          {profile.instruments.map((instrument, i) => <li key={i}>{instrument}</li>)}
+        </ul>
       </div>
-      <ul class="profile-instruments">
-        {
-          currentCandidate.instruments.map( (instrument, i) =>
-          <li key={i}>{ instrument }</li>)
-        }
-      </ul>
     </div>
-  </div>
-)
+  )
 }
--- src/components/Swipe/Swipe.js
+++ src/components/Swipe/Swipe.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import Profile from "../Profile/ProfileCard";
-
+import Profile from '../Profile/ProfileCard'
 
 export default function swipe(props) {
+  const currentCandidate = props.profile[props.currentCandidateId]
+  //.I will take care of this if statement etc later
+  if (!currentCandidate) return 'Loading'
   return (
     <div id={'swipe'}>
       {!props.currentUserId && !props.currentCandidateId && 'Loading'}
       <h1>Swipe page is rendered</h1>
-      <Profile profile={props.profile} userId={props.currentCandidateId}/>
-       <button onClick={() => props.setDislikeUser(props.currentUserId, props.currentCandidateId)}>Dislike</button>
-      <button onClick={() => props.setLikeUser(props.currentUserId, props.currentCandidateId)}>Like</button>
+      <Profile profile={currentCandidate} />
+      <button onClick={() => props.setDislikeUser(props.currentUserId, props.currentCandidateId)}>
+        Dislike
+      </button>
+      <button onClick={() => props.setLikeUser(props.currentUserId, props.currentCandidateId)}>
+        Like
+      </button>
     </div>
-   
-
   )
 }
