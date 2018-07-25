56629a018d5f6b05e30c133f813cae891e50ab6c
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-07-25 11:10:44 +0200

    matchedContainer and matchedPage

--- src/components/Matches/MatchesPage.js
+++ src/components/Matches/MatchesPage.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const MatchesPage = props => {
-  return (
-    <div>
-      <a href="https://wa.me/310627653060?text=I'm%20matched%20with%20you%20on%20Knakworst"><FontAwesomeIcon icon={['fab', 'whatsapp']} size="3x" /></a>
-
-    </div>
-  )
+const tempStyle = {
+  border: '2px solid gray',
+  borderRadius: '20px',
+  margin: '10%',
+  textAlign: 'center'
 }
 
-MatchesPage.propTypes = {
+const MatchesPage = props =>
+  props.matchedUsers.map(user => {
+    return (
+      <div key={user.id} style={tempStyle}>
+        <h1>{user.username}</h1>
+        <p>age:</p>
+        <p>{user.age}</p>
+        <hr />
+        <p>place:</p>
+        <p>{user.address}</p>
+        <hr />
+        <p>genre(s)</p>
+        {user.genres.map(genre => <p key={genre}>{genre}</p>)}
+        <hr />
+        <p>instrument(s)</p>
+        {user.instruments.map(instrument => <p key={instrument}>{instrument}</p>)}
+        <hr />
+        <a
+          href={`https://wa.me/${user.phone.replace(
+            /-/g,
+            ''
+          )}?text=I'm%20matched%20with%20you%20on%20Knakworst`}
+        >
+          <FontAwesomeIcon icon={['fab', 'whatsapp']} size="3x" />
+        </a>
+      </div>
+    )
+  })
 
-}
+MatchesPage.propTypes = {}
 
 export default MatchesPage
