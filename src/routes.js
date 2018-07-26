ba99f935213bb12db9ec60581c0a0a53528f1615
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-07-26 09:54:16 +0200

    fix duplication in the reducer and route path

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import {BrowserRouter, Route} from "react-router-dom"
-import MatchesPageContainer from "./components/Matches/MatchesPageContainer";
-import ProfileContainer from "./components/Profile/PersonalProfileContainer";
-import SwipeContainer from "./components/Swipe/SwipeContainer";
+import { BrowserRouter, Route } from 'react-router-dom'
+import MatchesPageContainer from './components/Matches/MatchesPageContainer'
+import ProfileContainer from './components/Profile/PersonalProfileContainer'
+import SwipeContainer from './components/Swipe/SwipeContainer'
 import EditPreferencesContainer from './components/EditPreferences/EditPreferencesPageContainer'
-import App from "./components/App/App";
+import App from './components/App/App'
 
 export default () => {
-    return(
-        <BrowserRouter>
-          <div>
-            <Route exact path={'/'} component={App} />
-            <Route exact path={'/matches'} component={MatchesPageContainer}/>
-            <Route exact path={'/profile'} component={ProfileContainer}/>
-            <Route exact path={'/swipe'} component={SwipeContainer}/>
-            <Route exact path={'/edit-profile'} component={EditPreferencesContainer}/>
-          </div>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path={'/'} component={App} />
+        <Route exact path={'/matches'} component={MatchesPageContainer} />
+        <Route exact path={'/profile/:id'} component={ProfileContainer} />
+        <Route exact path={'/swipe'} component={SwipeContainer} />
+        <Route exact path={'/edit-profile'} component={EditPreferencesContainer} />
+      </div>
+    </BrowserRouter>
+  )
 }
