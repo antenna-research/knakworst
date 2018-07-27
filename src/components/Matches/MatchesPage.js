39d443f976873c3d3cdcd29bfeafab86861f86c3
isabellabrookes <hello@isabellabrookes.com>
2018-07-27 10:53:47 +0200

    styling and logic for profile views

--- src/components/Matches/MatchesPage.js
+++ src/components/Matches/MatchesPage.js
@@ -1,41 +1,27 @@
-import { Col, Grid, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import Button from 'react-bootstrap/lib/Button'
 import React from 'react'
-
 import './styles/MatchesPage.scss'
 
-import PropTypes from 'prop-types'
 
 const MatchesPage = props => (
-  <div id="MachesPage">
+  <div id="MatchesPage">
     {props.matchedUsers.map(user => (
-      <Col xs={12} md={5} key={user.id} className="profile-wrapper">
-        <img src={user.image} alt="" />
-        <div className="info">
-          <h1 className="name">{user.username}</h1>
-          <div className="details">
-            <p>age: {user.age}</p>
-            <p>location: {user.address}</p>
-            <p>genre(s): {user.genres.map(genre => <span key={genre}>{genre} </span>)}</p>
-
-            <p>
-              instrument(s):{' '}
-              {user.instruments.map(instrument => <span key={instrument}>{instrument} </span>)}
-            </p>
-          </div>
-
+      <div key={user.id} className="MatchesPages-individualMatch-div">
+      <img src={user.image} alt="" />
+      <div id="user">
+        <h1 className="name">{user.username}, {user.age}</h1>
+        <div><FontAwesomeIcon icon={'map-marker-alt'}/>   {user.address}</div>
+      </div>
+        <div id="profile-button">
           <Link to={`/profile/${user.id}`}>
-            <Button className="button">Profile</Button>
+            <button className="button">Profile</button>
           </Link>
         </div>
-      </Col>
+      </div>
     ))}
   </div>
 )
 
-MatchesPage.propTypes = {}
 
 export default MatchesPage
--- src/components/Profile/PersonalProfileContainer.js
+++ src/components/Profile/PersonalProfileContainer.js
@@ -4,17 +4,19 @@ import {Link} from 'react-router-dom'
 import NavComponent from '../Nav/NavComponent'
 import Profile from './ProfileCard'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import './styles/PeronalProfileContainer.scss'
+import './styles/PersonalProfileContainer.scss'
 
 class PersonalProfileContainer extends PureComponent {
   render() {
     return (
       <div id={'PersonalProfileContainer'}>
         <NavComponent />
+        {this.props.currentUser === this.props.match.params.id ? (
         <div id='PersonalProfileContainerLinkDiv'>
           <Link to={`/profile/edit/${this.props.match.params.id}`} className={'Personal-Profile-Links'}><FontAwesomeIcon icon='user-edit' /></Link>
           <Link to={'/preferences'} className={'Personal-Profile-Links'}><FontAwesomeIcon icon='sliders-h' /></Link>
         </div>
+          ) : (null)}
         <Profile profile={this.props.viewedUser} />
       </div>
     )
