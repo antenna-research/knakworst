bfad3ee9db7c78f732b1280af30df5a79c48574a
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-07-27 15:41:09 +0200

    logout button

--- src/components/Profile/ProfileCard.js
+++ src/components/Profile/ProfileCard.js
@@ -1,32 +1,48 @@
-import React from 'react'
 import './styles/ProfileCard.scss'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+import LogoutButton from '../UI/LogoutButton'
 
 export default function ProfileCard({ profile }) {
   return (
     <div id={'Profile-component'}>
       <div id={'Profile-card'}>
         {!profile && 'Loading'}
-        <div className={"profile-video"}>
+        <div className={'profile-video'}>
           <iframe
-            src={ 'https://www.youtube.com/embed/' + profile.youtube }
+            src={'https://www.youtube.com/embed/' + profile.youtube}
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullScreen
           />
         </div>
         <div className={'profile-info'}>
-          <h1>{profile.username}, {profile.age}</h1>
-          <h2>aka {profile.firstName} {profile.lastName}</h2>
-          <div className={"profile-city"}><FontAwesomeIcon icon={'map-marker-alt'}/>  {profile.address}</div>
+          <h1>
+            {profile.username}, {profile.age}
+          </h1>
+          <h2>
+            aka {profile.firstName} {profile.lastName}
+          </h2>
+          <div className={'profile-city'}>
+            <FontAwesomeIcon icon={'map-marker-alt'} /> {profile.address}
+          </div>
           <ul>
-            {profile.genres.map((genre, i) => <li key={i} className={"profile-genres"}>{genre}</li>)}
+            {profile.genres.map((genre, i) => (
+              <li key={i} className={'profile-genres'}>
+                {genre}
+              </li>
+            ))}
           </ul>
-          <div className={"profile-instruments-div"}>
-            {profile.instruments.map((instrument, i) => <div key={i} className={"profile-instruments " + instrument}></div>)}
+          <div className={'profile-instruments-div'}>
+            {profile.instruments.map((instrument, i) => (
+              <div key={i} className={'profile-instruments ' + instrument} />
+            ))}
           </div>
         </div>
       </div>
+      <LogoutButton />
     </div>
   )
 }
