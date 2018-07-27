22b8e2636be01579e0dbf735d0c34e773b9ba9aa
timvandervoorn <tvanderv@gmail.com>
2018-07-27 13:51:41 +0200

    Added instr.

--- src/data/alternatives-per-preference.js
+++ src/data/alternatives-per-preference.js
@@ -1,4 +1,4 @@
-export const allGenres =  ['Rock', 'Jazz', 'Funk', 'Classic'];
-export const allInstruments = ['Piano', 'Bass', 'Flute']
-export const allLocations = ['Amsterdam', 'Leiden', 'Den Haag']
+export const allGenres =  ['Rock', 'Jazz', 'Funk', 'Classic', 'Pop', 'Reggae', 'SynthPop', 'Experimental'];
+export const allInstruments = ['Piano', 'Bass', 'Flute', 'Drums', 'Violin', 'Vocals', 'Cello','Contrabass', 'Tambourine']
+export const allLocations = ['Amsterdam', 'Leiden', 'Den Haag', 'Rotterdam']
 
